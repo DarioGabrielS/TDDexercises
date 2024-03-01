9f9847d8bd9fc66fbf5f91584b7c45abd7be3763
DarioGabrielS <dariogabriel.salvadore@gmail.com>
2024-03-01 19:11:58 -0300

    Reverse documented for Jsdoc 3

--- Exercises/Two/Reverser/reverser.js
+++ Exercises/Two/Reverser/reverser.js
@@ -1,9 +1,24 @@
+ /**
+ * Reverses a String.
+ *
+ * @param {string} str - String to reverse.
+ * @returns {string} - The string reversed.
+ * @throws {string} - An error message ig a string is not given or param is not a string type.
+ */
 
 function reverse (str) {
 
+/**
+* @throws {string} - Error message if string is not provided
+*/
    if(!str) return 'Must provide a String'
+/**
+* @throws {string} - Error message if string is not provided
+*/
    if(typeof str != 'string') return 'Parameter must be a string'
-
+/**
+* @returns - Parameter is splited character by character, reversed and then joined again before returning it  
+*/
     return str.split('').reverse().join('')
 
 }
