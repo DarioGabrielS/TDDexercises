1fd4801bd2570175ce79d642258142ead3d7ac86
DarioGabrielS <dariogabriel.salvadore@gmail.com>
2024-03-13 15:06:05 -0300

    Ranges

--- Exercises/Two/Ranges/Ranges.js
+++ Exercises/Two/Ranges/Ranges.js
@@ -1,44 +1,28 @@
+function displayRange(list) {
+  let ranges = "";
 
-function displayRange (list){
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] == list[i + 2] - 2) {
+      let initial = list[i];
+      let final = list[i + 1];
+      let j = i + 1;
 
-  let ranges = ''
-  
-  for ( let i = 0; i < list.length; i++){
-  	
-    
-    if(list[i] == list[i+2] - 2){
-        
-        let initial = list[i]
-        let final = list[i+1]
-      	let j = i+1
-   
-      	for(  j  ; j < list.length; j++){
-      	
-          
-            if(list[i] == list[j] -1){
-            
-              
-              final = list[j]
-              
-              i++
-              
-            }
-          	
-      
-    		}
-      
-      ranges = ranges.concat(`${initial}-${final},`) 
-      
-      } 
-      
-    else {
-    
-			ranges = ranges.concat(`${list[i]},`)
+      for (j; j < list.length; j++) {
+        if (list[i] == list[j] - 1) {
+          final = list[j];
+
+          i++;
+        }
+      }
+
+      ranges = ranges.concat(`${initial}-${final},`);
+    } else {
+      ranges = ranges.concat(`${list[i]},`);
     }
-	}
-  const rangeSplited = ranges.split('')
-  rangeSplited.pop()
-  const finalRange = rangeSplited.join('')
-  
-  return finalRange
+  }
+  const rangeSplited = ranges.split("");
+  rangeSplited.pop();
+  const finalRange = rangeSplited.join("");
+
+  return finalRange;
 }
